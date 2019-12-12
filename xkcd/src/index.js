c8cea42319dec4b9ec99e6022ca3256433ae04b5
Schrese <53787633+Schrese@users.noreply.github.com>
2019-12-12 16:54:02 -0500

    set up the initial reducer

--- xkcd/src/index.js
+++ xkcd/src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
- 
+
+//reducers
+import {reducer} from './reducers';
+
 const store = createStore(reducer)
 
 ReactDOM.render(
-    <Provider>
+    <Provider store = {store}>
         <App />
     </Provider>
 , document.getElementById('root'));
---
+++ xkcd/src/reducers/comicReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    comic: null, 
+    isFetching: false, 
+    error: ''
+}
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default: 
+            return state;
+    }
+}
